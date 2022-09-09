15ff6c2d7e06004b4ff53aa85ccb8d9deea55a05
Don Richards <highlander411@gmail.com>
2022-09-08 21:28:51 -0400

    Add dependency check for gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,12 @@ buildscript {
         maven {
             url = uri("https://plugins.gradle.org/m2/")
         }
+        mavenCentral()
     }
     dependencies {
         classpath("ca.islandora:isle-gradle-docker-plugin:0.0.1")
         classpath("com.palantir.gradle.gitversion:gradle-git-version:0.12.3")
+        classpath("org.owasp:dependency-check-gradle:7.1.2")
     }
 }
 
@@ -20,3 +22,4 @@ allprojects {
 }
 
 apply(plugin = "ca.islandora.gradle.docker")
+apply(plugin = "org.owasp.dependencycheck")
